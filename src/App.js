720e0ad4b08e61815b7b1e8cbc0f1654397acfbf
3l4x <c0ltwinters@protonmail.com>
2023-02-16 06:36:46 +0100

    Initial commit
    Create basic structure for project
    Add router and redux to dependencies
    Add basic routing inside App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+//import { connect } from 'react-redux'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+//views
+import * as Views from './views/index.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Views.Home/>}/>
+        <Route path='/register' element={<Views.Register/>}/>
+        <Route path='/login' element={<Views.Login/>}/>
+        <Route path='/checkout' element={<Views.Checkout/>}/>
+        <Route path='/products' element={<Views.Products/>}/>
+        <Route path='/cart' element={<Views.Cart/>}/>
+        <Route path='*' element={<Views.NotFound/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/views/index.js
@@ -0,0 +1,7 @@
+export {default as Home} from './Home';
+export {default as NotFound} from './NotFound';
+export {default as Register} from './Register';
+export {default as Login} from './Login';
+export {default as Products} from './Products';
+export {default as Checkout} from './Checkout';
+export {default as Cart} from './Cart';
