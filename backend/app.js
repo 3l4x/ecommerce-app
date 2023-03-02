63b577c92c9ab89420f191d2291b66f49fb2825f
3l4x <c0ltwinters@protonmail.com>
2023-03-02 05:05:18 +0100

    Add error handler middleware placeholder

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,20 @@ app.use('/api/auth', authRouter);
 app.use('/api/graphql', graphqlServer)
 
 
-
-//
-
-
+//TODO fix error handler middleware
+app.use((err, req, res, next) => {
+    if (!err)
+        next();
+    if (err.name === 'ValidationError')
+    //do stuff
+    { }
+    else if (err.name === 'something else')
+    //do stuff
+    { }
+    else
+        return res.status(500).json({
+            error: 'internal server error'
+        })
+});
 
 module.exports = app;
