15001cfcbbe4aa3e5e5fbfab53fe36d3b05c1fcd
3l4x <c0ltwinters@protonmail.com>
2023-03-04 06:44:35 +0100

    Add merging and loading for typeDefs and resolvers
    -Add dynamical loading for typeDefs with load-files from ./types/*.js
    -Add dynamical loading for resolvers with load-files from ./types/*.js
    -loadFilesSync uses customLoader for both
    -Merging both typeDefs and resolvers with graphql-tools/merge

--- backend/schemas/graphql/public/resolvers.js
+++ backend/schemas/graphql/public/resolvers.js
@@ -1,6 +1,9 @@
-const { resolvers : productResolvers } = require("./types/productType");
+const { loadFilesSync } = require('@graphql-tools/load-files');
+const { mergeResolvers } = require('@graphql-tools/merge')
+const {resolverLoader} = require('../../../utils/customLoader');
+const path = require('path');
 
+const resolversArray = loadFilesSync(path.join(__dirname,'./types'),{extensions: ['js'], extractExports: resolverLoader})
+const resolvers = mergeResolvers(resolversArray);
 
-module.exports = {
-    productResolvers
-}
+module.exports = resolvers;
--- backend/schemas/graphql/public/typeDefs.js
+++ backend/schemas/graphql/public/typeDefs.js
@@ -1,6 +1,9 @@
-const { typeDefs : productTypeDefs } = require("./types/productType");
+const { mergeTypeDefs } = require('@graphql-tools/merge')
+const { loadFilesSync } = require('@graphql-tools/load-files');
+const {typeDefLoader} = require('../../../utils/customLoader');
+const path = require('path');
 
+const typeDefsArray = loadFilesSync(path.join(__dirname,'./types'),{extensions: ['js'], extractExports: typeDefLoader})
+const typeDefs = mergeTypeDefs(typeDefsArray);
 
-module.exports = {
-    productTypeDefs
-}
+module.exports =  typeDefs;
